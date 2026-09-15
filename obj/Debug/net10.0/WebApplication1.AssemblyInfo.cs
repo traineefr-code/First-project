039ed97b5cb311439e3e562268ff892871b237e0
traineefr-code <ikechukwuojo23@gmail.com>
2026-09-15 12:26:04 +0100

    -minor changes

--- obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b86a90e218227acf918b43527d9aeeef6978904")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a69035d43822f41392a67ce42acb744c0b36031c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
