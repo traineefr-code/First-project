f65cc502f07bbcac0fd6442180db445aacbc8619
traineefr-code <ikechukwuojo23@gmail.com>
2026-09-15 10:32:39 +0100

    -removed conflicting content on homepage

--- obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72c34e21d1431e47b9c58bfe73bdfd8e10225128")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b86a90e218227acf918b43527d9aeeef6978904")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
